e5ddbc4e7f39fe56ca05a564c682343d47e93ce3
vinocturne <vinocturne@naver.com>
2021-05-03 23:49:41 +0900

    todolist 1차 완성

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,63 @@
 <template>
-  <div>
-      input
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+      <!-- <button v-on:click="addTodo">add</button> -->
+      <span class="addContainer" v-on:click="addTodo">
+          <i class="fas fa-plus-square addBtn"></i>
+      </span>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function(){
+            if(this.newTodoItem !== '') {
+                var obj = {completed: false, item: this.newTodoItem};
+                //저장하는 로직
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj)); 
+                //JSON.stringfy는 객체를 스트링화 시켜주는 것. 그냥 obj를 넣으면 변환이 안되서 localStorage에서 확인 불가능.
+                this.clearInput();
+            }
+            
+        },
+        clearInput: function() {
+            //input 박스 비워주기
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped> 
+/* scoped는 해당 뷰 파일에서만 스타일을 적용시킨다는 뜻 */
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
