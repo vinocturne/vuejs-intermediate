0e394e66a735e3ac4c9268486537813c8940c960
vinocturne <vinocturne@naver.com>
2021-05-04 15:56:04 +0900

    add Modal Component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,14 +5,27 @@
       <span class="addContainer" v-on:click="addTodo">
           <i class="fas fa-plus-square addBtn"></i>
       </span>
+      <Modal v-if="showModal" @close="showModal = false">
+        <!--
+        you can use custom content here to overwrite
+        default content
+        -->
+        <h3 slot="header">경고!
+            <i class="closeModalBtn fas fa-times" @click="showModal=false"></i>
+        </h3>
+
+        <p slot="body">내용을 입력해주세요</p>
+      </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
     data: function() {
         return {
-            newTodoItem: ""
+            newTodoItem: "",
+            showModal: false
         }
     },
     methods: {
@@ -21,6 +34,8 @@ export default {
                 // this.$emit('이벤트 이름', 인자1, 인자2, ...);
                 this.$emit('addTodoItem', this.newTodoItem);
                 this.clearInput();
+            }else {
+                this.showModal = !this.showModal;
             }
             
         },
@@ -28,6 +43,9 @@ export default {
             //input 박스 비워주기
             this.newTodoItem = '';
         }
+    },
+    components: {
+        Modal: Modal
     }
 }
 </script>
@@ -57,5 +75,8 @@ input:focus {
 .addBtn {
     color: white;
     vertical-align: middle;
+}
+.closeModalBtn {
+    color: #42b983;
 }
 </style>
