8bcf5f33bd36aa4eec118fd508a8504db0b79656
vinocturne <vinocturne@naver.com>
2021-05-07 17:52:40 +0900

    set Vuex store

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { store } from './store/store'
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
+  store, // store : store의 축약형
   render: h => h(App),
-}).$mount('#app')
+});
---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+//use는 Vue의 플러그인.
+//vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때.
+
+export const store = new Vuex.Store({
+    //Vuex등록
+    //main.js를 들고와서 등록해주어야 함.
+});
