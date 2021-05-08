384df220d21ca33a277a8027c21173b6cb1c5134
vinocturne <vinocturne@naver.com>
2021-05-08 17:18:15 +0900

    refactoring with modules

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,55 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import * as getters from './getters'
+// import * as mutations from './mutations'
+import todoApp from './modules/todoApp'
 
 Vue.use(Vuex);
 //use는 Vue의 플러그인.
 //vue를 사용하는 모든 영역에 특정 기능을 추가하고 싶을 때.
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if(localStorage.length > 0) {
-            for(let i=0; i<localStorage.length; i++) {
-                if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                    //    this.todoItems.push(localStorage.key(i));
-                }
-            }
-        }
-        return arr;    
-    }
-};
 
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch()
-    },
-    getters: {
-        storedTodoItems(state) {
-            return state.todoItems;
-        }
-    },
-    mutations: {
-        addOneItem(state, todoItem) {
-            const obj = {completed: false, item: todoItem};
-            localStorage.setItem(todoItem, JSON.stringify(obj)); 
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload) {
-            localStorage.removeItem(payload.todoItem.item);
-            state.todoItems.splice(payload.index, 1);
-        },
-        toggleOneItem(state, payload) {
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            localStorage.removeItem(payload.todoItem.item);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItems(state) {
-            localStorage.clear();
-            state.todoItems = [];
-        }
+    modules: {
+        todoApp
     }
-    
-    
-    
 });
